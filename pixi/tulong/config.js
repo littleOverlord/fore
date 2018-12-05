5e3a11e6cb4c4204a0ecd67061cabba53820d16c
tang2dan <tang2dan.gmail.com>
2018-12-05 08:46:10 +0800

    fixed cacl root size

--- pixi/tulong/config.js
+++ pixi/tulong/config.js
@@ -1,20 +1,22 @@
 export default class Config{
     constructor(option){
         const {pixelRatio, windowWidth, windowHeight} = option;
-        let w,h,
+        let w,h,l = 0,t = 0,
             sw = this.screen._width/windowWidth,
             sh = this.screen._height/windowHeight,
-            s = Math.min(sw,sh);
+            s = Math.max(sw,sh);
         w = windowWidth * s;
         h = windowHeight * s;
-        if(sw > s){
-            w = (windowWidth/windowHeight) * h;
-        }else if(sh > s){
-            h = (windowHeight/windowWidth) * w;
+        if(sw < s){
+            l = Math.floor((w - this.screen._width)/2);
+        }else if(sh < s){
+            t = Math.floor((h - this.screen._height)/2);
         }
 
         this.screen.width = w;
         this.screen.height = h;
+        this.screen.left = l;
+        this.screen.top = t;
         this.screen.scale = s;
     }
     screen = {
@@ -22,6 +24,8 @@ export default class Config{
         _height: 1334,
         width: 0,
         height: 0,
+        left: 0,
+        top: 0,
         scale: 1
     }
 }
