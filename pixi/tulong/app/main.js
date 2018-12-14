dad1aaff1fd489cd532123a76c8af998f81b8329
tdd <tdd_style@163.com>
2018-12-14 09:52:29 +0800

    add animate

--- pixi/tulong/app/main.js
+++ pixi/tulong/app/main.js
@@ -22,7 +22,7 @@ export default class Main {
                 }
             };
         console.log(cfg);
-        Scene.Application({
+        let app = Scene.Application({
             width: cfg.screen.width,
             height: cfg.screen.height,  
             antialias: true,
@@ -30,15 +30,16 @@ export default class Main {
             view: canvas,
             resolution: 1
         },cfg);
-        Loader.add(["images/ui/bag_bg.png","images/ui/bag_tab_armors.png","images/ui/bag_tab_attack.png","images/ui/bag_tab_bg.png","images/ui/bag_tab_curr.png","images/ui/button_buy.png","images/ui/button_sale.png","images/ui/button_store.png","images/ui/scene.png","images/ui/stage_level_bg.png","images/ui/token_attack.png","images/ui/token_bg.png","images/ui/token_diamond.png","images/ui/token_hp.png","images/ui/token_money.png","images/ani/B_S_005.png"],function(){
+        Loader.add(["images/ui.png","images/ani/B_S_005.png"],function(){
             loadCount -= 1;
             loadOk();
         });
-        Loader.loadJson(["images/ani/B_S_005.json"],function(res){
+        Loader.loadJson(["images/ani/B_S_005.json","images/ui.json"],function(res){
             console.log(res);
             jsonData = res;
             loadCount -= 1;
             loadOk();
         });
+        console.log(app);
     }
 }
