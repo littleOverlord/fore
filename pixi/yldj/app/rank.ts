0b0e6f8d2d8fc871afe8f0d2e1a940e66bfc30e9
tdd <tdd_style@163.com>
2019-09-11 15:21:18 +0800

    show rank list

--- pixi/yldj/app/rank.ts
+++ pixi/yldj/app/rank.ts
@@ -3,12 +3,20 @@ import Widget from '../libs/ni/widget';
 import DB from '../libs/ni/db';
 import { AppEmitter } from './appEmitter';
 import Connect from '../libs/ni/connect';
+import Scene from '../libs/ni/scene';
 
 /****************** 导出 ******************/
 
 /****************** 本地 ******************/
-let ranks = []
-
+/**
+ * ranksInfo
+ * {
+ *  "rank":[],
+ *  top:0
+ * }
+ */
+let ranksInfo;
+let rankNode;
 /**
  * @description 用户组件
  */
@@ -19,10 +27,27 @@ class WrankTop extends Widget{
         }
     }
     added(node){
-        if(ranks.length == 0){
-            return;
+        rankNode = node;
+        updateRankPage();
+    }
+}
+class WrankItem extends Widget{
+    setProps(props){
+        this.cfg.data.left = props.index * 90;
+        this.cfg.data.top = Math.floor(props.index / 6) * 200;
+        if(props.info.head){
+            this.cfg.children[1].data.netUrl = props.info.head;
+            delete this.cfg.children[1].data.url;
+        }else{
+            this.cfg.children[1].data.url = "images/logo/logo.png"
         }
-        node.children[0].alpha = 0;
+        if(props.info.name){
+            this.cfg.children[2].data.text = props.info.name;
+        }
+        this.cfg.children[0].data.text = `第${props.top}名`;
+        this.cfg.children[3].data.text = `${props.info.score}`;
+    }
+    added(node){
         // TODO 插入排行数据....
 
     }
@@ -36,6 +61,8 @@ const readScore = () => {
             return;
         }
         console.log(data);
+        DB.data.score.phase = data.ok.phase;
+        DB.data.score.history = data.ok.history;
     })
 }
 /**
@@ -63,13 +90,37 @@ const readRank = () => {
             return;
         }
         console.log(data);
+        ranksInfo = data.ok;
+        updateRankPage();
     })
+}
+/**
+ * @description 更新排行页面
+ */
+const updateRankPage = () => {
+    if(!rankNode || ranksInfo.rank.length == 0){
+        return;
+    }
+    let t;
+    rankNode.children[0].alpha = 0;
+    if(rankNode.children.length > 1){
+        rankNode.removeChildren(1);
+    }
+    // TODO 插入排行数据....
+    for(let i = 0, len = ranksInfo.rank.length; i < len; i++){
+        t = i+1;
+        if(i > 2){
+            t = i-2+ranksInfo.start +1;
+        }
+        Scene.open("app-ui-rank_item",rankNode,null,{"info":ranksInfo.rank[i],"top":t,"index":i})
+    }
 }
 /****************** 立即执行 ******************/
 //初始化关卡数据库表
 DB.init("score",{history:0,phase:0});
 //注册组件
 Widget.registW("app-ui-rank_top",WrankTop);
+Widget.registW("app-ui-rank_item",WrankItem);
 AppEmitter.add("gameStart",()=>{
     readScore();
     readRank();
